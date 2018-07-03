7b97d5ef56b9fd1f0d1f885d4e9c675a75671c41
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 00:28:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
-var playlist = { `hozier`: `sedated` };
+var playlist = new Object{
+  hozier: "sedated" 
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle });
